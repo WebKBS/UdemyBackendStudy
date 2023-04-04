2315f918f4ca5173b38d8515e6e084d624c696cc
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-04 22:45:19 +0900

    express 초기설정

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toLocaleDateString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hi</h1>");
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequest);
+app.get("/currenttime", (req, res) => {
+  res.send("<h1>" + new Date().toLocaleDateString() + "</h1>");
+});
 
-server.listen(3000);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello!!</h1>");
+});
+
+app.listen(3000);
+
+//const http = require("http");
+
+// function handleRequest(request, response) {
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200;
+//     response.end("<h1>" + new Date().toLocaleDateString() + "</h1>");
+//   } else if (request.url === "/") {
+//     response.statusCode = 200;
+//     response.end("<h1>Hi</h1>");
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+//server.listen(3000);
