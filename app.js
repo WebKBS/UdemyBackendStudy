78bade2741003d702e1e72f792a99b9f6529fa00
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-05 22:43:37 +0900

    express form 전송방법 get, post, use()

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); //use를 사용하지 않으면 form post전송시 데이터가 전송되지 않는다.
+
 app.get("/currenttime", (req, res) => {
   res.send("<h1>" + new Date().toLocaleDateString() + "</h1>");
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello!!</h1>");
+  res.send(
+    "<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>"
+  );
+});
+
+// input에는 반드시 name을 입력해야 하며 req.body를 통해서 name속성을 가져올 수 있다.
+app.post("/store-user", (req, res) => {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h1>UserName Success!</h1>");
 });
 
 app.listen(3000);
