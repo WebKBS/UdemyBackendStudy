bbcc4691c9ed24e5b93e5a5c02a705bd96a90be7
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-08 18:40:59 +0900

    node post를 통해 JSON 파일 저장하는 방법

--- 00-starting-project/app.js
+++ 00-starting-project/app.js
@@ -1,8 +1,11 @@
+const fs = require("fs");
+
 const path = require("path");
 const express = require("express");
 const app = express();
 
 app.use(express.static("public")); // css 및 javascript를 사용하려면 반드시 써줘야한다.
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -17,6 +20,19 @@ app.get("/restaurants", (req, res) => {
 app.get("/recommend", (req, res) => {
   const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", (req, res) => {
+  const restaurant = req.body; //요청
+  const filePath = path.join(__dirname, "data", "restaurants.json"); // 파일 저장된 위치
+
+  const fileData = fs.readFileSync(filePath); // 파일 읽기
+  const storeRestaurants = JSON.parse(fileData); // JSON으로 전환
+  storeRestaurants.push(restaurant); // 파일 푸시
+
+  fs.writeFileSync(filePath, JSON.stringify(storeRestaurants)); // 파일 쓰기
+  console.log(restaurant);
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", (req, res) => {
