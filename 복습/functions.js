e24868458dde52dd13926138838bb0412e12c371
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-13 21:51:27 +0900

    스프레드 연산자 사용

--- 복습/functions.js
+++ 복습/functions.js
@@ -2,5 +2,28 @@ const greetUser = (greetingPrefix, userName = "default") => {
   console.log(greetingPrefix + " " + "Hello" + " " + userName);
 };
 
-greetUser("Hi");
-greetUser();
+// greetUser("Hi");
+// greetUser();
+
+// function sum(numbers) {
+//   let result = 0;
+//   for (const num of numbers) {
+//     result += num;
+//   }
+//   return result;
+// }
+
+// console.log(sum([1, 2, 3]));
+
+function sum(...numbers) {
+  let result = 0;
+
+  for (const num of numbers) {
+    result += num;
+  }
+  return result;
+}
+
+const inputNumvers = [1, 2, 5, 6, 87, 22];
+
+console.log(sum(...inputNumvers));
