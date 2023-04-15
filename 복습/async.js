f5375411106e1c0a966e5bbd4424919ea0ddfaf8
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-15 23:06:30 +0900

    node promises 에러 처리하기

--- 복습/async.js
+++ 복습/async.js
@@ -3,13 +3,21 @@ const { addSyntheticLeadingComment } = require("typescript");
 
 const readFile = () => {
   // fs.readFile("data.txt", (error, fileData) => {
+  //if(error){
+  //
+  //} // 에러개 발생했을때
   //   console.log("hi");
   //   console.log(fileData.toString()); // 읽은 파일을 문자열로 바꿔준다.
   // });
 
-  fs.readFile("data.txt").then((fileData) => {
-    console.log(fileData.toString());
-  });
+  fs.readFile("data.txt")
+    .then((fileData) => {
+      console.log(fileData.toString());
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("파일 누락");
+    });
 
   console.log("Hi there!");
 };
