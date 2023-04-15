4b441b9a2f432168153db6050f4edc8dcfc81bf4
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-15 23:02:12 +0900

    node 에서 promises 사용법

--- 복습/async.js
+++ 복습/async.js
@@ -1,9 +1,14 @@
-const fs = require("fs");
+const fs = require("fs/promises");
+const { addSyntheticLeadingComment } = require("typescript");
 
 const readFile = () => {
-  fs.readFile("data.txt", (error, fileData) => {
-    console.log("hi");
-    console.log(fileData.toString()); // 읽은 파일을 문자열로 바꿔준다.
+  // fs.readFile("data.txt", (error, fileData) => {
+  //   console.log("hi");
+  //   console.log(fileData.toString()); // 읽은 파일을 문자열로 바꿔준다.
+  // });
+
+  fs.readFile("data.txt").then((fileData) => {
+    console.log(fileData.toString());
   });
 
   console.log("Hi there!");
