ad5528100205636fb5b833ea98a7f232fa649a31
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-15 23:34:08 +0900

    async await 에러처리

--- 복습/async.js
+++ 복습/async.js
@@ -9,9 +9,13 @@ const readFile = async () => {
   //   console.log(fileData.toString()); // 읽은 파일을 문자열로 바꿔준다.
   // });
 
-  const fileData = await fs.readFile("data.txt");
-
-  console.log(fileData.toString());
+  try {
+    const fileData = await fs.readFile("data.txt");
+    console.log(fileData.toString());
+  } catch (error) {
+    console.log(error);
+    console.log("파일에서 에러가 발생");
+  }
 
   console.log("Hi there!");
 };
