c44d21914682838f445d1ea0e6fcad1aa6f913e6
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-28 00:43:33 +0900

    혹시나 모를 에러처리

--- Node_MongoDB/routes/blog.js
+++ Node_MongoDB/routes/blog.js
@@ -48,8 +48,16 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts/:id", async (req, res) => {
-  const postId = req.params.id;
+router.get("/posts/:id", async (req, res, next) => {
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render("404");
+    //return next(error);
+  }
+
   const post = await db
     .getDb()
     .collection("posts")
