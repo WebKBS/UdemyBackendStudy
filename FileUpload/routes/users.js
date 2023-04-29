e70ccd5c1882543d379fa3f6b617826adf97d49f
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-29 21:59:12 +0900

    이미지 업로드 하기

--- FileUpload/routes/users.js
+++ FileUpload/routes/users.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const multer = require("multer");
 
-const upload = multer({});
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cd) {
+    cd(null, "images");
+  },
+  filename: function (req, file, cd) {
+    cd(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
@@ -16,6 +25,11 @@ router.get("/new-user", function (req, res) {
 router.post("/profiles", upload.single("image"), (req, res) => {
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect("/");
 });
 
 module.exports = router;
