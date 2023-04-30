ee04021349c5fc6d4d7206a8a021e5b278686cdb
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-30 13:50:28 +0900

    프론트 데이터 가져와서 화면에 추가하기

--- Ajax/public/scripts/comments.js
+++ Ajax/public/scripts/comments.js
@@ -1,4 +1,27 @@
 const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentsSectionElement = document.getElementById("comments");
+
+const createCommentsList = (comments) => {
+  const commentListElement = document.createElement("ol");
+
+  console.log(comments.comments);
+
+  for (const comment of comments.comments) {
+    console.log(comment);
+
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `
+    <article class="comment-item">
+      <h2>${comment.title}</h2>
+      <p>${comment.text}</p>
+    </article>
+    `;
+
+    commentListElement.appendChild(commentElement);
+  }
+
+  return commentListElement;
+};
 
 const fetchCommentsForPost = async (event) => {
   const postId = loadCommentsBtnElement.dataset.postid;
@@ -6,7 +29,11 @@ const fetchCommentsForPost = async (event) => {
   const response = await fetch(`/posts/${postId}/comments`);
   const responseData = await response.json();
 
-  console.log(responseData.comments);
+  //console.log(responseData.comments);
+
+  const commentsListElement = createCommentsList(responseData);
+  commentsSectionElement.innerHTML = "";
+  commentsSectionElement.appendChild(commentsListElement);
 };
 
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
