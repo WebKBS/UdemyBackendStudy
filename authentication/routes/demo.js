4c5173d9c5f24d69be91d5f1c410141e3fd1d2bd
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-30 20:32:31 +0900

    signup post 만들기

--- authentication/routes/demo.js
+++ authentication/routes/demo.js
@@ -1,29 +1,43 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", async function (req, res) {});
+
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email; // userData['email']
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
+
+  console.log(userData);
+
+  const user = {
+    email: enteredEmail,
+    password: enteredPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/login", async function (req, res) {});
 
-router.post('/login', async function (req, res) {});
-
-router.get('/admin', function (req, res) {
-  res.render('admin');
+router.get("/admin", function (req, res) {
+  res.render("admin");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
