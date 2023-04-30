024ea8c883123302bd4c0183f08a2109eec7789b
BeomSu-Kang <kbs21c1987@gmail.com>
2023-04-30 21:16:33 +0900

    bcrypt를 사용해서 비밀번호 해시화 하기

--- authentication/routes/demo.js
+++ authentication/routes/demo.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const db = require("../data/database");
 
@@ -8,7 +9,9 @@ router.get("/", function (req, res) {
   res.render("welcome");
 });
 
-router.get("/signup", async function (req, res) {});
+router.get("/signup", async function (req, res) {
+  res.render("signup");
+});
 
 router.get("/login", function (req, res) {
   res.render("login");
@@ -20,11 +23,13 @@ router.post("/signup", async function (req, res) {
   const enteredConfirmEmail = userData["confirm-email"];
   const enteredPassword = userData.password;
 
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
   console.log(userData);
 
   const user = {
     email: enteredEmail,
-    password: enteredPassword,
+    password: hashedPassword,
   };
 
   await db.getDb().collection("users").insertOne(user);
@@ -32,7 +37,36 @@ router.post("/signup", async function (req, res) {
   res.redirect("/login");
 });
 
-router.post("/login", async function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email; // userData['email']
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  // 유저가 없는 경우
+  if (!existingUser) {
+    console.log("로그인 할수 없습니다.");
+    return res.redirect("/login");
+  }
+
+  // bcrypt를 사용해서 password가 일치하는지 검증한다.
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordsAreEqual) {
+    console.log("패스워드가 다릅니다!!");
+    return res.redirect("/login");
+  }
+
+  console.log("로그인 성공!!");
+  res.redirect("/admin");
+});
 
 router.get("/admin", function (req, res) {
   res.render("admin");
